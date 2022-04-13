70769f0c15e72d940280b33bad24b89ddf3d1134
IdaStyrbaek <97097737+IdaStyrbaek@users.noreply.github.com>
2022-04-13 11:00:04 +0200

    All sound files

---
+++ all_files.js
@@ -0,0 +1,603 @@
+/* create variable containing the files*/
+var sound_files = [
+    {
+      "sound": "animal-animals-E.mp3"
+    },
+    {
+      "sound": "animal-bear-baere-E.mp3"
+    },
+    {
+      "sound": "animal-bees-E.mp3"
+    },
+    {
+      "sound": "animal-biscuit-beskidt-D.mp3"
+    },
+    {
+      "sound": "animal-cockroaches-E.mp3"
+    },
+    {
+      "sound": "animal-deer-de-E.mp3"
+    },
+    {
+      "sound": "animal-dogs-E.mp3"
+    },
+    {
+      "sound": "animal-donkey-E.mp3"
+    },
+    {
+      "sound": "animal-duckling-E.mp3"
+    },
+    {
+      "sound": "animal-duen-D.mp3"
+    },
+    {
+      "sound": "animal-elefant-D.mp3"
+    },
+    {
+      "sound": "animal-flu-flue-D.mp3"
+    },
+    {
+      "sound": "animal-fool-fugle-D.mp3"
+    },
+    {
+      "sound": "animal-fox-E.mp3"
+    },
+    {
+      "sound": "animal-giraf-D.mp3"
+    },
+    {
+      "sound": "animal-graevling-D.mp3"
+    },
+    {
+      "sound": "animal-gris-D.mp3"
+    },
+    {
+      "sound": "animal-guldfisk-D.mp3"
+    },
+    {
+      "sound": "animal-hamster-D.mp3"
+    },
+    {
+      "sound": "animal-hest-D.mp3"
+    },
+    {
+      "sound": "animal-hippo-E.mp3"
+    },
+    {
+      "sound": "animal-hoens-D.mp3"
+    },
+    {
+      "sound": "animal-horses-horsens-E.mp3"
+    },
+    {
+      "sound": "animal-hund-D.mp3"
+    },
+    {
+      "sound": "animal-kaeledyr-D.mp3"
+    },
+    {
+      "sound": "animal-kanin-D.mp3"
+    },
+    {
+      "sound": "animal-katten-D.mp3"
+    },
+    {
+      "sound": "animal-killing-kaelling-E.mp3"
+    },
+    {
+      "sound": "animal-koeer-D.mp3"
+    },
+    {
+      "sound": "animal-loeve-D.mp3"
+    },
+    {
+      "sound": "animal-mow-maage-D.mp3"
+    },
+    {
+      "sound": "animal-mus-D.mp3"
+    },
+    {
+      "sound": "animal-myg-D.mp3"
+    },
+    {
+      "sound": "animal-parrots-E.mp3"
+    },
+    {
+      "sound": "animal-peacock-E.mp3"
+    },
+    {
+      "sound": "animal-pheasants-E.mp3"
+    },
+    {
+      "sound": "animal-pigs-E.mp3"
+    },
+    {
+      "sound": "animal-pin-pind-D.mp3"
+    },
+    {
+      "sound": "animal-platypus-E.mp3"
+    },
+    {
+      "sound": "animal-racoons-E.mp3"
+    },
+    {
+      "sound": "animal-rats-E.mp3"
+    },
+    {
+      "sound": "animal-regnorm-D.mp3"
+    },
+    {
+      "sound": "animal-rotter-D.mp3"
+    },
+    {
+      "sound": "animal-skunk-E.mp3"
+    },
+    {
+      "sound": "animal-spider-spejder-E.mp3"
+    },
+    {
+      "sound": "animal-squirrel-E.mp3"
+    },
+    {
+      "sound": "animal-svane-D.mp3"
+    },
+    {
+      "sound": "animal-toad-E.mp3"
+    },
+    {
+      "sound": "animal-wolves-E.mp3"
+    },
+    {
+      "sound": "animal-zebra-E.mp3"
+    },
+    {
+      "sound": "food-aeblekage-D.mp3"
+    },
+    {
+      "sound": "food-aeg-D.mp3"
+    },
+    {
+      "sound": "food-bacon-E.mp3"
+    },
+    {
+      "sound": "food-bear-baere-D.mp3"
+    },
+    {
+      "sound": "food-biscuit-beskidt-E.mp3"
+    },
+    {
+      "sound": "food-boef-D.mp3"
+    },
+    {
+      "sound": "food-bread-E.mp3"
+    },
+    {
+      "sound": "food-broccoli-D.mp3"
+    },
+    {
+      "sound": "food-burger-E.mp3"
+    },
+    {
+      "sound": "food-cake-E.mp3"
+    },
+    {
+      "sound": "food-carrots-E.mp3"
+    },
+    {
+      "sound": "food-chips-D.mp3"
+    },
+    {
+      "sound": "food-chips-E.mp3"
+    },
+    {
+      "sound": "food-chokolade-D.mp3"
+    },
+    {
+      "sound": "food-chokoladekage-D.mp3"
+    },
+    {
+      "sound": "food-cookie-kuk-E.mp3"
+    },
+    {
+      "sound": "food-deer-de-D.mp3"
+    },
+    {
+      "sound": "food-easy-is-D.mp3"
+    },
+    {
+      "sound": "food-flaeskesteg-D.mp3"
+    },
+    {
+      "sound": "food-fried rice-E.mp3"
+    },
+    {
+      "sound": "food-frikadeller-D.mp3"
+    },
+    {
+      "sound": "food-hotdogs-E.mp3"
+    },
+    {
+      "sound": "food-hotwings-E.mp3"
+    },
+    {
+      "sound": "food-kartoffelmos-D.mp3"
+    },
+    {
+      "sound": "food-lasagne-D.mp3"
+    },
+    {
+      "sound": "food-leverpostej-D.mp3"
+    },
+    {
+      "sound": "food-lower-laage-D.mp3"
+    },
+    {
+      "sound": "food-mars-bar-D.mp3"
+    },
+    {
+      "sound": "food-muffins-E.mp3"
+    },
+    {
+      "sound": "food-mushrooms-E.mp3"
+    },
+    {
+      "sound": "food-nudler-D.mp3"
+    },
+    {
+      "sound": "food-omelette-E.mp3"
+    },
+    {
+      "sound": "food-pandekager-D.mp3"
+    },
+    {
+      "sound": "food-pasta-E.mp3"
+    },
+    {
+      "sound": "food-peanuts-E.mp3"
+    },
+    {
+      "sound": "food-pea-pige-E.mp3"
+    },
+    {
+      "sound": "food-pie-pege-E.mp3"
+    },
+    {
+      "sound": "food-pizza-E.mp3"
+    },
+    {
+      "sound": "food-poelseret-D.mp3"
+    },
+    {
+      "sound": "food-pomfritter-D.mp3"
+    },
+    {
+      "sound": "food-poor-porre-D.mp3"
+    },
+    {
+      "sound": "food-risengroed-D.mp3"
+    },
+    {
+      "sound": "food-rugbroed-D.mp3"
+    },
+    {
+      "sound": "food-salad-E.mp3"
+    },
+    {
+      "sound": "food-scones-E.mp3"
+    },
+    {
+      "sound": "food-spaghetti-D.mp3"
+    },
+    {
+      "sound": "food-spies-spege-D.mp3"
+    },
+    {
+      "sound": "food-tacos-E.mp3"
+    },
+    {
+      "sound": "food-vaffelis-D.mp3"
+    },
+    {
+      "sound": "food-waffles-E.mp3"
+    },
+    {
+      "sound": "none-astrology-E.mp3"
+    },
+    {
+      "sound": "none-blade-D.mp3"
+    },
+    {
+      "sound": "none-breathe-E.mp3"
+    },
+    {
+      "sound": "none-bulletin-E.mp3"
+    },
+    {
+      "sound": "none-cookie-kuk-D.mp3"
+    },
+    {
+      "sound": "none-dancing-E.mp3"
+    },
+    {
+      "sound": "none-driving-E.mp3"
+    },
+    {
+      "sound": "none-drukkultur-D.mp3"
+    },
+    {
+      "sound": "none-easy-is-E.mp3"
+    },
+    {
+      "sound": "none-en skrue-D.mp3"
+    },
+    {
+      "sound": "none-expectations-E.mp3"
+    },
+    {
+      "sound": "none-far-far-D.mp3"
+    },
+    {
+      "sound": "none-fodbold-D.mp3"
+    },
+    {
+      "sound": "none-for helvede-D.mp3"
+    },
+    {
+      "sound": "none-Frozen-E.mp3"
+    },
+    {
+      "sound": "none-genbrugstoej-D.mp3"
+    },
+    {
+      "sound": "none-gymnastics-E.mp3"
+    },
+    {
+      "sound": "none-horses-horsens-D.mp3"
+    },
+    {
+      "sound": "none-jul-D.mp3"
+    },
+    {
+      "sound": "none-kender-D.mp3"
+    },
+    {
+      "sound": "none-kigger-D.mp3"
+    },
+    {
+      "sound": "none-koncert-D.mp3"
+    },
+    {
+      "sound": "none-loud music-E.mp3"
+    },
+    {
+      "sound": "none-lower-laage-E.mp3"
+    },
+    {
+      "sound": "none-makeup-E.mp3"
+    },
+    {
+      "sound": "none-movies-E.mp3"
+    },
+    {
+      "sound": "none-mow-maage-E.mp3"
+    },
+    {
+      "sound": "none-nailpolish-E.mp3"
+    },
+    {
+      "sound": "none-oede oe-D.mp3"
+    },
+    {
+      "sound": "none-Olsen banden-D.mp3"
+    },
+    {
+      "sound": "none-pick-pik-D.mp3"
+    },
+    {
+      "sound": "none-pie-pege-D.mp3"
+    },
+    {
+      "sound": "none-piercings-E.mp3"
+    },
+    {
+      "sound": "none-pinligt-D.mp3"
+    },
+    {
+      "sound": "none-pin-pind-E.mp3"
+    },
+    {
+      "sound": "none-regnvejr-D.mp3"
+    },
+    {
+      "sound": "none-ringenes herre-D.mp3"
+    },
+    {
+      "sound": "none-Roskilde-D.mp3"
+    },
+    {
+      "sound": "none-skating-E.mp3"
+    },
+    {
+      "sound": "none-smykker-D.mp3"
+    },
+    {
+      "sound": "none-snapchat-E.mp3"
+    },
+    {
+      "sound": "none-toy-toej-E.mp3"
+    },
+    {
+      "sound": "none-trimming-E.mp3"
+    },
+    {
+      "sound": "none-TV show-E.mp3"
+    },
+    {
+      "sound": "none-TV-E.mp3"
+    },
+    {
+      "sound": "none-udklaedning-D.mp3"
+    },
+    {
+      "sound": "none-vandpyt-D.mp3"
+    },
+    {
+      "sound": "none-Viborg-D.mp3"
+    },
+    {
+      "sound": "none-wedding-E.mp3"
+    },
+    {
+      "sound": "none-yoga-E.mp3"
+    },
+    {
+      "sound": "people-Americans-E.mp3"
+    },
+    {
+      "sound": "people-Anders-D.mp3"
+    },
+    {
+      "sound": "people-bartenderen-D.mp3"
+    },
+    {
+      "sound": "people-beyonce-E.mp3"
+    },
+    {
+      "sound": "people-buschauffoeren-D.mp3"
+    },
+    {
+      "sound": "people-comedians-E.mp3"
+    },
+    {
+      "sound": "people-dame-D.mp3"
+    },
+    {
+      "sound": "people-drengene-D.mp3"
+    },
+    {
+      "sound": "people-dronningen-D.mp3"
+    },
+    {
+      "sound": "people-everyone-E.mp3"
+    },
+    {
+      "sound": "people-far-far-E.mp3"
+    },
+    {
+      "sound": "people-farmor-D.mp3"
+    },
+    {
+      "sound": "people-flu-flue-E.mp3"
+    },
+    {
+      "sound": "people-folk-D.mp3"
+    },
+    {
+      "sound": "people-folkeskoleelever-D.mp3"
+    },
+    {
+      "sound": "people-fool-fugle-E.mp3"
+    },
+    {
+      "sound": "people-girl-E.mp3"
+    },
+    {
+      "sound": "people-guy-E.mp3"
+    },
+    {
+      "sound": "people-immigrants-E.mp3"
+    },
+    {
+      "sound": "people-indvandrere-D.mp3"
+    },
+    {
+      "sound": "people-influencers-E.mp3"
+    },
+    {
+      "sound": "people-Kardashian-E.mp3"
+    },
+    {
+      "sound": "people-kids-E.mp3"
+    },
+    {
+      "sound": "people-killing-kaelling-D.mp3"
+    },
+    {
+      "sound": "people-klasselaerer-D.mp3"
+    },
+    {
+      "sound": "people-lillebror-D.mp3"
+    },
+    {
+      "sound": "people-little girl-E.mp3"
+    },
+    {
+      "sound": "people-manden-D.mp3"
+    },
+    {
+      "sound": "people-man-E.mp3"
+    },
+    {
+      "sound": "people-matros-D.mp3"
+    },
+    {
+      "sound": "people-models-E.mp3"
+    },
+    {
+      "sound": "people-old people-E.mp3"
+    },
+    {
+      "sound": "people-onkel-D.mp3"
+    },
+    {
+      "sound": "people-passengers-E.mp3"
+    },
+    {
+      "sound": "people-pea-pige-D.mp3"
+    },
+    {
+      "sound": "people-pick-pik-E.mp3"
+    },
+    {
+      "sound": "people-politikere-D.mp3"
+    },
+    {
+      "sound": "people-poor-porre-E.mp3"
+    },
+    {
+      "sound": "people-president-E.mp3"
+    },
+    {
+      "sound": "people-princess-E.mp3"
+    },
+    {
+      "sound": "people-prins-D.mp3"
+    },
+    {
+      "sound": "people-queen-E.mp3"
+    },
+    {
+      "sound": "people-some people-E.mp3"
+    },
+    {
+      "sound": "people-spider-spejder-D.mp3"
+    },
+    {
+      "sound": "people-spies-spege-E.mp3"
+    },
+    {
+      "sound": "people-statsministeren-D.mp3"
+    },
+    {
+      "sound": "people-teacher-E.mp3"
+    },
+    {
+      "sound": "people-this guy-E.mp3"
+    },
+    {
+      "sound": "people-toeserne-D.mp3"
+    },
+    {
+      "sound": "people-toy-toej-D.mp3"
+    }
+  ]
